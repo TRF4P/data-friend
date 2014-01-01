4831a4414e1758f5994b31d9612b9362bb99eb79
clarknoah <noahb08@gmail.com>
2014-01-01 17:49:04 -0500

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('dataAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
