9a58c920ef86eb7540fb89ef492e23ed7a96a9b6
clarknoah <noahb08@gmail.com>
2014-01-02 10:17:21 -0500

    before heroku

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,12 +4,21 @@ angular.module('dataAppApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'dataAppApp.services'
 ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/dataModel', {
+        templateUrl: 'views/dataModel.html',
+        controller: 'MainCtrl'
+      })
+      .when('/dataBuilder', {
+        templateUrl: 'views/dataBuilder.html',
         controller: 'MainCtrl'
       })
       .otherwise({
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('dataAppApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http, dataService) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+    /** dataService.loadConfig('/scripts/model/model.json')
+    .then(
+      function (data) {
+        $scope.nodes = data.rootNodeModel;
+        $scope.rels = data.rootRelationshipModel;
+        //$scope.sample = $scope.samples[0];
+      },
+      function (error) {
+        $scope.error = error;
+      }
+    ); **/
+    
   });
